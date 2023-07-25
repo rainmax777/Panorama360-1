96f2d2a2ad3dd1bb352ce30038f68643ac0f1de2
rainmax777 <88648153+rainmax777@users.noreply.github.com>
2023-07-25 22:53:13 +0300

    Upload

--- data.js
+++ data.js
@@ -16,12 +16,60 @@ var APP_DATA = {
       ],
       "faceSize": 512,
       "initialViewParameters": {
+        "yaw": 1.3350335799998696,
         "pitch": 0,
-        "yaw": 0,
+        "fov": 1.7607213063833194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5240182624575613,
+          "pitch": 0.010762027661893825,
+          "rotation": 3.9269908169872414,
+          "target": "1---05_camera001_07-25-2023"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---05_camera001_07-25-2023",
+      "name": "Люба Гостинная Бар05_Camera001_07-25-2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.09399132743556038,
+        "pitch": -0.09527361957979785,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": -1.3200050055223613,
+          "pitch": -0.0014902983785667345,
+          "rotation": 0,
+          "target": "0-untitled_perspective_07-24-2023_01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8661950811874526,
+          "pitch": -0.31482726727332633,
+          "title": "Это кухня<br>",
+          "text": "Армстронг"
+        }
+      ]
     }
   ],
   "name": "Project Title",
